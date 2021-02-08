b8be357c64632eaf5fc6cf2600aa7803d9f55e58
Gabriel Blanco La Rotta <geblarot96@gmail.com>
2021-02-08 14:41:22 -0500

    Initial commit

---
+++ src/main/java/com/gabensoft/helloworld2/BasicController.java
@@ -0,0 +1,13 @@
+package com.gabensoft.helloworld2;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BasicController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "index";
+    }
+}
