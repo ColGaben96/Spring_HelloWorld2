7d9dbb9ddb0a1d1bb2f8184535acd0bec3f6b527
Gabriel Blanco La Rotta <geblarot96@gmail.com>
2021-02-08 14:51:35 -0500

    Kinda works | Gabriel Blanco

--- src/main/java/com/gabensoft/helloworld2/BasicController.java
+++ src/main/java/com/gabensoft/helloworld2/BasicController.java
@@ -1,13 +1,16 @@
 package com.gabensoft.helloworld2;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@Slf4j
 public class BasicController {
 
-    @GetMapping("/")
+    @GetMapping("/test")
     public String hello() {
+        log.info("Entrando al controlador");
         return "index";
     }
 }
